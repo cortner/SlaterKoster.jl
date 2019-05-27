07de3f3eedf3253cdac7dd0421f9d005cf323a38
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-05-27 22:09:23 +0100

    some notes to work from

--- src/SlaterKoster.jl
+++ src/SlaterKoster.jl
@@ -1,6 +1,18 @@
 module SlaterKoster
 
+# Functionality we need - by order of
+#  (1) correct s, p, d orbitals
+#      INPUT: U, hop -> OUTPUT: matrix
+#  (2) AD derivatives
+#        - w.r.t. U
+#        - w.r.t. hop
+#        all further derivatives are handles outside
+#  (3) correct f and higher orbitals
+#  (4) fast evaluation
+#  (5) fast derivatives
 
+
+# my old codes from the previous TB package
 include("oldsk.jl")
 
 end # module
