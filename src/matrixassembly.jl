5a38cdbd14da0a2b5aa6ee61d5812371432d36b6
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-06-05 17:28:26 +0100

    backup

--- src/matrixassembly.jl
+++ src/matrixassembly.jl
@@ -14,6 +14,8 @@ mutable struct OffsiteHamiltonianRI{T}
    Nat::Int
 end
 
+Base.length(H::OffsiteHamiltonianRI) = length(H.Iat)
+
 
 # ----------------------------------------------------------------
 #    Two-Centre Case
@@ -93,4 +95,75 @@ function _assembleRI!(Iat, Jat, R, V, nlist, model, temp)
       end
    end
    return V
+end
+
+
+
+# ----------------------------------------------------------------
+#    Assemble the Hamiltonian Matrix in Cartesian format
+
+get_norbloc(H::SKH) = sum( 2*get_l(o)+1 for o in H.orbitals )
+
+global_index(iat, iloc, nat, norbloc) = (iat-1) * norbloc .+ iloc
+
+function Base.Matrix(HRI::OffsiteHamiltonianRI{T}) where {T}
+   norbloc = get_norbloc(HRI.skh)
+   norbglob = HRI.Nat * norbloc
+
+   # allocate the global hamiltonian matrix
+   H = zeros(norbglob, norbglob)
+
+   for nb = 1:length(H.skh.orbitals)
+      # call the function barrier
+      _assemble_full_inner!(H, HRI, nb, b, ...todo..., norbloc) # TODO make sure these indices are SVectors
+   end
+
+   return H
+end
+
+function _assemble_full_inner!(H, HRI, nb, b, irowloc, jcolloc, norbloc)
+   sig = sksign(b)
+   sigt = sksignt(b)
+   for (n, (iat, jat, R)) in enumerate( zip(HRI.Iat, HRI.Jat, HRI.R) )
+      V = HRI.V[n, nb]
+      U = R / norm(R)
+      φ, θ = carttospher(U[1], U[2], U[3])
+      E12 = CodeGeneration.sk_gen(b, φ, θ)
+      irowglob = global_index(iat, irowloc, HRI.Nat, norbloc)
+      jcolglob = global_index(jat, jcolloc, HRI.Nat, norbloc)
+      @. H[irowglob, jcolglob] = sig * V * E12
+      # + the symmetrised variations....
+   end
+end
+
+
+
+# TODO:
+# - k-dependence
+# - overlaps
+
+
+function Base.Matrix(H::OffsiteHamiltonianRI{ORB, T}) where {ORB, T}
+   # total number of orbitals = size of matrix
+   norb_local = length(H.orbitals)
+   norb_global = H.Nat * norb_local
+   Hout = zeros(T, norb_global, norb_global)
+   Hloc = zeros(T, norb_local, norb_loca)
+   # loop over all bond-integrals between any two atoms
+   for (iat, jat, R, V, bond_type) in H
+      @assert jat > iat # assume this convention for now
+      # assemble the relevant sk block and the associated _local_ matrix Hloc
+      # (this includes the symmetries within the local block!)
+      # sig ∈ {1, -1} is the sign of the transposed element
+      irow, jcol, sig = sk_block!(Hloc, R, bond_type, H.orbitals)
+      # get the global indices from the local and the atom indices
+      irowg = global_indices(H.orbitals, iat, irow)
+      jcolg = global_indices(H.orbitals, jat, jcol)
+      # write information into global H
+      for (ig, jg, i, j) in zip(irowg, jcolg, irow, jcol)
+         H[ig, jg] += V * Hloc[i, j]
+         H[jg, ig] += sig * V * Hloc[i, j]
+      end
+   end
+   return H
 end
